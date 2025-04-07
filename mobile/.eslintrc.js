3bad234f88d8cb567ed4435d1549720c13afc995
jugurtamahious <jugurtamahious@gmail.com>
2025-04-07 17:48:02 +0200

    feat prettier

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,5 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: [
+    'expo',
+    'plugin:prettier/recommended', // ➕ ajoute Prettier
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error', // ➕ affiche une erreur si le format ne respecte pas prettier
+  },
   ignorePatterns: ['/dist/*'],
 };
