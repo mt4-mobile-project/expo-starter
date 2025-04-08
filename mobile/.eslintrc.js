10053155a88b36ef381c0f1f5492ac2761e1ff62
Victor Huang <huang.victor.mail@gmail.com>
2025-04-08 02:07:05 +0200

    refacto : fix src

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -3,10 +3,33 @@ module.exports = {
   extends: [
     'expo',
     'plugin:prettier/recommended', // ➕ ajoute Prettier
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'import'],
   rules: {
     'prettier/prettier': 'error', // ➕ affiche une erreur si le format ne respecte pas prettier
+    'import/no-unresolved': 'error',
   },
   ignorePatterns: ['/dist/*'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: '/Users/vicoh/Documents/projects/nunu-mobile-app/mobile/tsconfig.json',
+      },
+      'babel-module': {
+        alias: {
+          '@': './src',
+        },
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
