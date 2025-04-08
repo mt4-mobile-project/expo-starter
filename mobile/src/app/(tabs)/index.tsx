4a8887ee57c1525f84afbb6f29da783fc42d1eda
Victor Huang <huang.victor.mail@gmail.com>
2025-04-08 03:22:58 +0200

    fix : button style

--- mobile/src/app/(tabs)/index.tsx
+++ mobile/src/app/(tabs)/index.tsx
@@ -1,16 +1,25 @@
 import { Button } from '@/components/atoms/buttons/button';
-import { View, Text } from 'tamagui';
+import { View } from 'tamagui';
 
 export default function HomeScreen() {
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
   console.log('API URL:', apiUrl);
 
   return (
-    <View>
-      <Text>Heh</Text>
+    <View
+      flex={1}
+      backgroundColor="$background"
+      padding="$4"
+      justifyContent="center"
+      alignItems="center"
+      gap="$4"
+    >
       <Button>Click me</Button>
       <Button variant="secondary">Click me</Button>
       <Button variant="outline">Click me</Button>
+      <Button variant="link">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="destructive">Click me</Button>
     </View>
   );
 }
