1ecce8ddfc41f48805c00155035d7e2ed99c4873
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2025-04-08 10:34:04 +0200

    Style: create component (atomes) and variants for input

--- mobile/src/app/(tabs)/index.tsx
+++ mobile/src/app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/atoms/buttons/button';
+import { Input } from '@/components/atoms/inputs/input';
 import { View } from 'tamagui';
 
 export default function HomeScreen() {
@@ -14,14 +15,21 @@ export default function HomeScreen() {
       alignItems="center"
       gap="$4"
     >
-      <Button>Click me</Button>
+      {/*       <Button>Click me</Button>
       <Button variant="secondary" size="lg">
         Click me
       </Button>
       <Button variant="outline">Click me</Button>
       <Button variant="link">Click me</Button>
       <Button variant="ghost">Click me</Button>
-      <Button variant="destructive">Click me</Button>
+      <Button variant="destructive">Click me</Button> */}
+
+      <Input placeholder="Default input" />
+      <Input variant="outline" placeholder="Outline variant" />
+      <Input variant="filled" placeholder="Filled variant" />
+      <Input size="sm" placeholder="Small size" />
+      <Input size="lg" placeholder="Large size" />
+      <Input disabled placeholder="Disabled input" />
     </View>
   );
 }
