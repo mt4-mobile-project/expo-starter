ef686a9e106bf458ea7e16a946134f0934eae564
Victor Huang <huang.victor.mail@gmail.com>
2025-04-09 05:20:36 +0200

    refacto : move set async storage auth token to auth hook

--- mobile/src/services/auth.ts
+++ mobile/src/services/auth.ts
@@ -1,13 +1,6 @@
 import { LoginCredentials, LoginResponse } from '@/types/login';
 import { api } from '@/utils/api';
-import { asyncStorageToken } from '@/utils/asyncStorageToken';
 
 export const login = async (credentials: LoginCredentials): Promise<LoginResponse> => {
-  const response = await api.post<LoginResponse>('/auth/login', credentials);
-
-  if (response.token) {
-    await asyncStorageToken.set(response.token);
-  }
-
-  return response;
+  return await api.post<LoginResponse>('/auth/login', credentials);
 };
