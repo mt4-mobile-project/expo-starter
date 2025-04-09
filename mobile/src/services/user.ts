21c1b24c61e650f0cc9c47945372943e7c404264
jugurtamahious <jugurtamahious@gmail.com>
2025-04-09 03:09:25 +0200

    feat filter users by username

--- mobile/src/services/user.ts
+++ mobile/src/services/user.ts
@@ -1,10 +1,22 @@
 import { api } from '@/utils/api';
 import { User } from '@/types/user';
+export const getUsers = async (searchTerm: string = ''): Promise<User[]> => {
+  const url = searchTerm ? `/users/username/${searchTerm}` : '/users';
+  console.log('Request URL:', url);
 
-export const getUsers = async (): Promise<User[]> => {
-  return api.get<User[]>('/users');
+  const response = await api.get(url);
+  console.log('API response:', response);
+
+  if (Array.isArray(response)) {
+    return response as User[];
+  }
+
+  return [response as User];
 };
+
+
 
 export const getCurrentUser = async (): Promise<User> => {
   return await api.get<User>('/users/me');
 };
+  
