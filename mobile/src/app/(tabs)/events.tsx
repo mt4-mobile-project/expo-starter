9d9b8ea8dffc9d5f1dcb8b1ba511152ec7150a72
Victor Huang <huang.victor.mail@gmail.com>
2025-04-09 23:37:20 +0200

    feat : handle images in events list

--- mobile/src/app/(tabs)/events.tsx
+++ mobile/src/app/(tabs)/events.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from 'tamagui';
+import { Text, View, ScrollView } from 'tamagui';
 import { Keyboard } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Input } from '@/components/atoms/inputs/input';
 import { Button } from '@/components/atoms/buttons/button';
-import { useEvents } from '@/hooks/useEvent';
+import { useEvents } from '@/hooks/events/useEvents';
 import { EventCard } from '@/components/molecules/event-card/event-card';
 import { useEventFilters } from '@/hooks/events/useEventFilters';
+import { useEffect } from 'react';
 
 export default function HomeScreen() {
   const { data: events, isLoading, error } = useEvents();
@@ -26,53 +27,59 @@ export default function HomeScreen() {
           ? 'Rechercher par adresse'
           : 'Rechercher';
 
+  useEffect(() => {
+    console.log('filteredEvents', filteredEvents[1]);
+  }, [filteredEvents]);
+
   return (
-    <View flex={1} backgroundColor="$background" padding="$4" gap="$4">
-      <Input
-        placeholder={placeholder}
-        variant="outline"
-        size="lg"
-        value={searchTerm}
-        onChangeText={setSearchTerm}
-        onSubmitEditing={handleSearchSubmit}
-        returnKeyType="search"
-        icon={<FontAwesome name="search" size={18} color="#aaa" />}
-      />
-
-      {/* Filter buttons */}
-      <View flexDirection="row" gap="$2">
-        <Button
-          variant={activeFilter === 'title' ? 'secondary' : 'outline'}
-          onPress={() => setActiveFilter('title')}
-        >
-          Par titre
-        </Button>
-        <Button
-          variant={activeFilter === 'date' ? 'secondary' : 'outline'}
-          onPress={() => setActiveFilter('date')}
-        >
-          Par date
-        </Button>
-        <Button
-          variant={activeFilter === 'address' ? 'secondary' : 'outline'}
-          onPress={() => setActiveFilter('address')}
-        >
-          Par adresse
-        </Button>
-      </View>
-
-      {/* Results section */}
-      {isLoading && <Text>Chargement des événements...</Text>}
-      {error && <Text>Erreur lors du chargement.</Text>}
-      {filteredEvents.map((event) => (
-        <EventCard
-          key={event.id}
-          imageUrl="https://picsum.photos/300"
-          title={event.name}
-          address={`${event.address.street}, ${event.address.city}`}
-          datetime={event.start_date}
+    <ScrollView>
+      <View flex={1} backgroundColor="$background" padding="$4" gap="$4">
+        <Input
+          placeholder={placeholder}
+          variant="outline"
+          size="lg"
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+          onSubmitEditing={handleSearchSubmit}
+          returnKeyType="search"
+          icon={<FontAwesome name="search" size={18} color="#aaa" />}
         />
-      ))}
-    </View>
+
+        {/* Filter buttons */}
+        <View flexDirection="row" gap="$2">
+          <Button
+            variant={activeFilter === 'title' ? 'secondary' : 'outline'}
+            onPress={() => setActiveFilter('title')}
+          >
+            Par titre
+          </Button>
+          <Button
+            variant={activeFilter === 'date' ? 'secondary' : 'outline'}
+            onPress={() => setActiveFilter('date')}
+          >
+            Par date
+          </Button>
+          <Button
+            variant={activeFilter === 'address' ? 'secondary' : 'outline'}
+            onPress={() => setActiveFilter('address')}
+          >
+            Par adresse
+          </Button>
+        </View>
+
+        {/* Results section */}
+        {isLoading && <Text>Chargement des événements...</Text>}
+        {error && <Text>Erreur lors du chargement.</Text>}
+        {filteredEvents.map((event) => (
+          <EventCard
+            key={event.id}
+            image={event.image || ''}
+            title={event.name}
+            address={`${event.address.street}, ${event.address.city}`}
+            datetime={event.start_date}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
