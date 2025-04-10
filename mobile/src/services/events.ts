eadc7292aa1882eea5cb40aae651d07dfeea19d8
Victor Huang <huang.victor.mail@gmail.com>
2025-04-10 17:14:35 +0200

    feat : create event

--- mobile/src/services/events.ts
+++ mobile/src/services/events.ts
@@ -5,3 +5,21 @@ export const getAllEvents = async (): Promise<Event[]> => {
   const response = await api.get<Event[]>('/events');
   return response;
 };
+
+export interface CreateEventDTO {
+  name: string;
+  description: string;
+  start_date: string;
+  end_date: string;
+  address: {
+    street: string;
+    city: string;
+    latitude: number;
+    longitude: number;
+  };
+}
+
+export const createEvent = async (data: CreateEventDTO): Promise<Event> => {
+  const response = await api.post<Event>('/events', data);
+  return response;
+};
