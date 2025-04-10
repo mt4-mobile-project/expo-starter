93936ec34b795646016f4b22f680f79d1fcc4628
Victor Huang <huang.victor.mail@gmail.com>
2025-04-11 01:37:50 +0200

    fix : remove console.log

--- mobile/src/hooks/auth/useAuth.ts
+++ mobile/src/hooks/auth/useAuth.ts
@@ -10,7 +10,6 @@ export const useAuth = () => {
     mutationFn: (data: LoginCredentials) => login(data),
     onSuccess: async (response: LoginResponse) => {
       if (response.token) {
-        console.log(response.token, 'kkkkkkkkkkkkk');
         await asyncStorageToken.set(response.token);
         try {
           console.log('Vérification du profil...');
