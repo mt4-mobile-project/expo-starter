0430d21fd2d461b46fb2d14f3c1afedce435fd4b
Rayane <mabroukr1997@gmail.com>
2025-04-10 15:43:11 +0200

    feat: enhance RoomScreen to handle message parsing and display

--- mobile/src/app/room/[id].tsx
+++ mobile/src/app/room/[id].tsx
@@ -3,21 +3,44 @@ import { View, Paragraph } from 'tamagui';
 import { useContext, useEffect, useState } from 'react';
 import { SocketContext } from '@/context/socket';
 import { asyncStorageToken } from '@/utils/asyncStorageToken';
+import Message from '@/components/molecules/messages/message';
 
 export default function RoomScreen() {
   const { id } = useLocalSearchParams();
   const socket = useContext(SocketContext);
   const [messages, setMessages] = useState<any>([]);
+  const [sender, setSender] = useState<any>(null);
+
+  function formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+    return date.toLocaleDateString('fr-FR', options);
+  }
+
   useEffect(() => {
     if (!socket) return;
 
     const subscribeToRoom = async () => {
       const token = await asyncStorageToken.get();
 
+      if (!token) return;
+
+      const base64Payload = token?.split('.')[1]; // partie 2 du token
+      const payload = JSON.parse(atob(base64Payload.replace(/-/g, '+').replace(/_/g, '/')));
+      
+      setSender(payload.sub);
+
       socket.subscribe(
         `/topic/room.${id}`,
         (message) => {
-          setMessages((prev: any) => [...prev, JSON.stringify(message)]);
+          console.log('message', message.body)
+          setMessages((prev: any) => [...prev, JSON.parse(message.body)]);
         },
         {
           Authorization: `Bearer ${token}`,
@@ -33,14 +56,24 @@ export default function RoomScreen() {
       flex={1}
       backgroundColor="$background"
       padding="$4"
-      justifyContent="center"
-      alignItems="center"
+      width={"100%"}
       gap="$4"
     >
       <Paragraph>Room n° {id}</Paragraph>
-      <Paragraph>
-        {messages.length > 0 ? JSON.stringify(messages) : 'y a pas de messages ici'}
-      </Paragraph>
+
+      {messages.length > 0 ? (
+        messages.map((message: any) => (
+          <Message
+            key={message.id}
+            id={message.id}
+            content={message.content}
+            date={formatDate(message.created_at)}
+            whoAreYou={sender === message.username ? 'sender' : 'receiver'}
+          />
+        ))
+      ) : (
+        <Paragraph>y a pas de messages ici</Paragraph>
+      )}
     </View>
   );
 }
